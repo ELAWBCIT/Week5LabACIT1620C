f5d3f0b2afcadd17b60cf1b3f8a88912d6873686
Ernest Law <elawvc@gmail.com>
2023-03-13 13:46:41 -0700

    Phase 7-1 inputted JavaScript folder and file, added objects and array as well as user prompt

---
+++ scripts/script.js
@@ -0,0 +1,51 @@
+let course1 = {
+    code: 1620,
+    name: "Webdev",
+    instructor: "Neda",
+}
+
+let course2 = {
+    code: 1420,
+    name: "Sysadmin",
+    instructor: "Frederic", 
+}
+
+let course3 = {
+    code: 1630, 
+    name: "Database", 
+    instructor: "Ben"
+}
+
+const courseList = [course1, course2, course3];
+
+let user = parseInt(prompt("Enter a course code"));   
+
+let taco = true;
+
+while (taco) { 
+    if ((!isNaN(user)) && (user >= 1000) && (user <= 9999)){ 
+        taco = false;
+
+        let exists = false;
+        for (let i = 0; i < courseList.length; i++) {
+            let codex = courseList[i] 
+                if (codex.code === user) { exists = true;
+                    console.log('Yes I am taking the course ' + codex.code + ' ' + codex.name)
+                    break;
+                } 
+        }
+        if (exists === false){
+            let course = {
+                code: user,
+                name: null,
+            } 
+            courseList.push(course);
+            console.log(courseList)
+            console.log('Successfully added')
+        }   
+    } else {
+        taco = false; 
+        console.log('Invalid input');
+    }
+}
+console.log(courseList)
